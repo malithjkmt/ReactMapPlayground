39e57889578c71122941af5c8d8cd57f1cfdd20d
Malith <malit.tilak@gmail.com>
2016-09-11 08:17:56 +0530

    Reset to basic Map

--- Map.js
+++ Map.js
@@ -6,16 +6,12 @@ import {
     Dimensions
 } from 'react-native';
 
-import flagBlueImg from './img/flag-blue.png';
-import flagPinkImg from './img/flag-pink.png';
-
 const {width, height} = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
 const LATITUDE = 6.833140;
 const LONGITUDE = 79.882270;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-const SPACE = 0.01;
 
 class Map extends Component {
 
@@ -33,7 +29,6 @@ class Map extends Component {
 
     onRegionChange(region) {
         this.setState({region});
-        console.log(region);
     }
 
     render() {
@@ -42,25 +37,6 @@ class Map extends Component {
                 style={ styles.map }
                 region={this.state.region}
                 onRegionChange={region => this.onRegionChange(region)}>
-
-                <MapView.Marker
-                    coordinate={{
-                        latitude: LATITUDE + SPACE,
-                        longitude: LONGITUDE + SPACE,
-                    }}
-                    centerOffset={{x: -18, y: -60}}
-                    anchor={{x: 0.69, y: 1}}
-                    image={flagBlueImg}>
-                </MapView.Marker>
-
-                <MapView.Marker
-                    coordinate={{
-                        latitude: LATITUDE - SPACE,
-                        longitude: LONGITUDE - SPACE,
-                    }}
-                    centerOffset={{x: -42, y: -60}}
-                    anchor={{x: 0.84, y: 1}}
-                    image={flagPinkImg}/>
             </MapView>
         );
     }
