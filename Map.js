df22ecd6f4fff80346482f01c0b66e7e778a9f2f
Malith <malit.tilak@gmail.com>
2016-09-11 08:10:05 +0530

    Add method to generate markers with random colors

--- Map.js
+++ Map.js
@@ -3,11 +3,10 @@ import MapView from 'react-native-maps';
 import React, {Component} from 'react';
 import {
     StyleSheet,
-    Dimensions
+    Dimensions,
+    View,
+    Text
 } from 'react-native';
-
-import flagBlueImg from './img/flag-blue.png';
-import flagPinkImg from './img/flag-pink.png';
 
 const {width, height} = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -15,8 +14,11 @@ const LATITUDE = 6.833140;
 const LONGITUDE = 79.882270;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-const SPACE = 0.01;
+let id = 0;
 
+function randomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 class Map extends Component {
 
     constructor() {
@@ -27,52 +29,81 @@ class Map extends Component {
                 longitude: LONGITUDE,
                 latitudeDelta: LATITUDE_DELTA,
                 longitudeDelta: LONGITUDE_DELTA,
-            }
+            },
+            markers: []
         };
+    }
+
+    onMapPress(e) {
+        this.setState({
+            markers: [
+                ...this.state.markers,   // previous markers
+                {                        // new marker
+                    coordinate: e.nativeEvent.coordinate,
+                    key: id++,
+                    color: randomColor(),
+                },
+            ],
+        });
+        console.log(this.state.markers);
     }
 
     onRegionChange(region) {
         this.setState({region});
-        console.log(region);
     }
 
     render() {
         return (
-            <MapView
-                style={ styles.map }
-                region={this.state.region}
-                onRegionChange={region => this.onRegionChange(region)}>
+            <View style={styles.container}>
+                <MapView
+                    style={ styles.map }
+                    region={this.state.region}
+                    onRegionChange={region => this.onRegionChange(region)}
+                    onPress={(e) => this.onMapPress(e)}>
 
-                <MapView.Marker
-                    coordinate={{
-                        latitude: LATITUDE + SPACE,
-                        longitude: LONGITUDE + SPACE,
-                    }}
-                    centerOffset={{x: -18, y: -60}}
-                    anchor={{x: 0.69, y: 1}}
-                    image={flagBlueImg}>
-                </MapView.Marker>
+                    {this.state.markers.map(marker => (
+                        <MapView.Marker
+                            key={marker.key}
+                            coordinate={marker.coordinate}
+                            pinColor={marker.color}
+                        />
+                    ))}
+                </MapView>
 
-                <MapView.Marker
-                    coordinate={{
-                        latitude: LATITUDE - SPACE,
-                        longitude: LONGITUDE - SPACE,
-                    }}
-                    centerOffset={{x: -42, y: -60}}
-                    anchor={{x: 0.84, y: 1}}
-                    image={flagPinkImg}/>
-            </MapView>
+                <View style={styles.buttonContainer}>
+                    <View style={styles.bubble}>
+                        <Text>Tap to create a marker of random color</Text>
+                    </View>
+                </View>
+            </View>
+
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
     map: {
         position: 'absolute',
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
+    },
+    bubble: {
+        backgroundColor: 'rgba(255,255,255,0.7)',
+        paddingHorizontal: 18,
+        paddingVertical: 12,
+        borderRadius: 20,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginVertical: 15,
+        backgroundColor: 'transparent',
     }
 });
 
