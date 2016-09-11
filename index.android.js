7bcb7dec39be0351cbb94a63bca304c46db6e17c
Malith <malit.tilak@gmail.com>
2016-09-11 10:25:27 +0530

    Cached Map example

--- index.android.js
+++ index.android.js
@@ -11,23 +11,10 @@ class MyApp extends Component {
 
   render() {
     return (
-      <View style = {styles.container}>
       <Map />
-      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  }
-});
 
 AppRegistry.registerComponent('MyApp', () => MyApp);
